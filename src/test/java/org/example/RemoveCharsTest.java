84e95e76f6cda48622392fcc81d2ca8e64366204
Cuninka Pavol <pavol.cuninka@icloud.com>
2023-11-08 12:46:32 +0100

    added test for RemoveCharsTest

---
+++ src/test/java/org/example/RemoveCharsTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class RemoveCharsTest {
+    @Test
+    public void testRemoval() {
+        assertEquals("loquen", RemoveChars.remove("eloquent"));
+        assertEquals("ountr", RemoveChars.remove("country"));
+        assertEquals("erso", RemoveChars.remove("person"));
+        assertEquals("lac", RemoveChars.remove("place"));
+    }
+}
