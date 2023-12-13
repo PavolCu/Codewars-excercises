85b4d1adc9beecc6f28e9e8e74821bc937c1c53a
Pavol Cuninka <pavol.cuninka@icloud.com>
2023-12-13 17:31:16 +0100

    StringUtils

---
+++ src/main/java/org/example/StringUtils.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class StringUtils {
+    public static String toAlternativeString(String string) {
+        char[] chars = string.toCharArray();
+        for(int i = 0; i < chars.length; i++) {
+            if(Character.isUpperCase(chars[i])) {
+                chars[i] = Character.toLowerCase(chars[i]);
+            } else if(Character.isLowerCase(chars[i])) {
+                chars[i] = Character.toUpperCase(chars[i]);
+            }
+        }
+        return new String(chars);
+    }
+}
