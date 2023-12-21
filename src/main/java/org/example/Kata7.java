57222c2e4042274be20232fa3c3bed5d88188e44
Pavol Cuninka <pavol.cuninka@icloud.com>
2023-12-21 22:21:06 +0100

    Kata7 block comment

--- src/main/java/org/example/Kata7.java
+++ src/main/java/org/example/Kata7.java
@@ -2,6 +2,10 @@ package org.example;
 
 import java.util.stream.IntStream;
 
+/*
+Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).  Examples  n = 0 ==> [1] # [2^0] n = 1 ==> [1, 2] # [2^0, 2^1] n = 2 ==> [1, 2, 4] # [2^0, 2^1, 2^2]
+ */
+
 public class Kata7 {
     public static long[] powersOfTwo(int n){
 
